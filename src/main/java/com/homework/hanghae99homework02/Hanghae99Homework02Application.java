1cef4ae459707b4ccf7e8033b95d231b18616e66
ojy9612 <pvnm@naver.com>
2022-06-10 17:06:53 +0900

    프로젝트 생성

---
+++ src/main/java/com/homework/hanghae99homework02/Hanghae99Homework02Application.java
@@ -0,0 +1,13 @@
+package com.homework.hanghae99homework02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99Homework02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hanghae99Homework02Application.class, args);
+    }
+
+}
